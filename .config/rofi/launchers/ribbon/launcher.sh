b401465f0b8f93f3929dd4ad87612c4f5312ca3b
Oleksandr Nazaruk <oleksandrnazarukk@gmail.com>
2021-10-05 00:23:22 +0300

    Clean up + picom conf

--- .config/rofi/launchers/ribbon/launcher.sh
+++ .config/rofi/launchers/ribbon/launcher.sh
@@ -4,8 +4,6 @@ dir="$HOME/.config/rofi/launchers/ribbon"
 styles=($(ls -p --hide="colors.rasi" $dir/styles))
 color="theme.rasi"
 
-# comment this line to disable random colors
 sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
-
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
